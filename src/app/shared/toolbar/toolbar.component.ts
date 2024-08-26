d725275150d68e4296a06f2885b891cca9320e89
Angel Ochoa <aochoa@chartis.com>
2024-08-26 08:13:08 -0300

    ► Toolbar update by rol.

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,11 +1,28 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { AuthService } from '../../services/auth.service'
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
   @Input() nombreEmpleado: string = ''
   @Input() titulo: string = ''
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    if (this.authService.userToken?.rol === 'usuario') {
+      this.titulo = 'Gestion de Empresa - Lista de Empleados'
+      this.nombreEmpleado = `${this.authService.userToken?.email}`
+    } else {
+      this.titulo = 'Gestion de Empresa - Perfil'
+      this.nombreEmpleado = `${this.authService.userToken?.email}`
+    }
+  }
+
+  cerrarSesion() {
+    this.authService.logout()
+  }
 }
